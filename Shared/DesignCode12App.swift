88f764cd085cbe1c16f661d05e6dbce658403f7c
Robert Pelz <pelz.robert@gmail.com>
2020-08-29 18:14:49 +0200

    Initial Commit

---
+++ Shared/DesignCode12App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode12App.swift
+//  Shared
+//
+//  Created by Robert Pelz on 29.08.20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
