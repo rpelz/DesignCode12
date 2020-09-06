338d48f439049cca0eca2a60bd7719eccdd00c66
Robert Pelz <rpelz@icloud.com>
2020-09-06 21:37:13 +0200

    add some animation to the navigation

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct CoursesView: View {
+    @State var show = false
+    @Namespace var namespace
+    
     var body: some View {
-        Text("Content goes here!")
+        ZStack {
+            CourseItem()
+                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
+                .frame(width:335, height: 250)
+            if show {
+                ScrollView {
+                    CourseItem()
+                        .matchedGeometryEffect(id: "Card", in: namespace)
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            CourseRow()
+                        }
+                    }
+                    .padding()
+                }
+                .transition(.opacity)
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
+        //.animation(.spring())
+        .onTapGesture {
+            withAnimation(.spring()) {
+                show.toggle()
+            }
+        }
     }
 }
 
