e41032f496c35538c7278f2fd2c2f8944cf67565
Robert Pelz <rpelz@icloud.com>
2020-09-10 23:48:34 +0200

    Add visual blur effect for overlay

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -15,47 +15,65 @@ struct CoursesView: View {
     
     var body: some View {
         ZStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
-                    ForEach(courses) { item in
-                        VStack {
-                            CourseItem(course: item)
-                                .frame(height: 200)
-                                .onTapGesture {
-                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
-                                        show.toggle()
-                                        selectedItem = item
-                                        isDisabled = true
-                                    }
-                                }
-                                .disabled(isDisabled)
-                        }
-                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
-                    }
-                }
-                .padding(16)
-                .frame(maxWidth: .infinity)
-            }
-            .zIndex(1)
-            if selectedItem != nil {
-                ZStack (alignment: .topTrailing){
-                    CourseDetail(course: selectedItem!, namespace: namespace)
-                    CloseButton()
-                        .padding(16)
-                        .onTapGesture {
-                            withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
-                                show.toggle()
-                                selectedItem = nil
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    isDisabled = false
-                                }
-                                
-                            }
-                        }
-                }
-                .zIndex(2)
-            }
+            #if os(iOS)
+            content
+            fullContent
+                .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
+            #else
+            content
+            fullContent
+                .background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
+            #endif
         }
+    }
+    
+    @ViewBuilder
+    var fullContent: some View {
+        if selectedItem != nil {
+            ZStack (alignment: .topTrailing){
+                CourseDetail(course: selectedItem!, namespace: namespace)
+                CloseButton()
+                    .padding(16)
+                    .onTapGesture {
+                        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                isDisabled = false
+                            }
+                            
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    var content: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
+                ForEach(courses) { item in
+                    VStack {
+                        CourseItem(course: item)
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
+                                }
+                            }
+                            .disabled(isDisabled)
+                    }
+                    .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
+                }
+            }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+        }
+        .zIndex(1)
     }
 }
 
